bdb07cdad5fabd8b65aa677a7ae4983b2cef3a69
Sara Aumiller <sara.aumiller@cern.ch>
2024-07-18 15:17:47 +0200

    yeay worked out how to use singularity!

--- submit_condor_gpu/submitTrainingJob.sh
+++ submit_condor_gpu/submitTrainingJob.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
-NUM_GPU=${1}
-NUM_EPOCH=${2} # default 40
+NUM_GPU=${1:-1}
+NUM_EPOCH=${2:-40} # default 40
 
 start_time=$(date +%s)
 
-# source env/docker
-echo "Activating singularity..."
-singularity shell -B /eos -B /afs --nv /afs/cern.ch/work/s/saaumill/public/colorsinglet.sif
-echo "Activated!"
+#singularity exec -B /eos -B /afs --nv /afs/cern.ch/work/s/saaumill/public/colorsinglet.sif 
 
 # overcome RAM limitations when running locally - needed???
 #ulimit -S -s unlimited
 
 # start training
+dirpath="/afs/cern.ch/work/s/saaumill/public"
 datapath="/eos/experiment/fcc/ee/datasets/CLD_fullsim_tagging_input/*.root"
 
 if [ $NUM_GPU -eq 1 ]; then # train on one gpu
-    python3 -m ./../weaver.train --data-train $datapath --data-config ./../configs/example.yaml --network-config ./../../particle_transformer/networks/example_ParticleTransformer.py --model-prefix ./model_weights/ --num-workers 0 --gpus 0 --batch-size 2048 --start-lr 1e-3 --num-epochs $NUM_EPOCH --optimizer ranger --fetch-step 0.01  --log-wandb --wandb-displayname test_fullsim_CLD --wandb-projectname CLD_FullSim_tagging --lr-scheduler reduceplateau
+    singularity exec -B /eos -B /afs --nv /afs/cern.ch/work/s/saaumill/public/colorsinglet.sif python3 "${dirpath}/weaver-core/weaver/train.py" --data-train $datapath --data-config "${dirpath}/weaver-core/configs/example.yaml" --network-config "${dirpath}/particle_transformer/networks/example_ParticleTransformer.py" --model-prefix ./model_weights/ --num-workers 0 --gpus 0 --batch-size 2048 --start-lr 1e-3 --num-epochs $NUM_EPOCH --optimizer ranger --fetch-step 0.01  --log-wandb --wandb-displayname test_fullsim_CLD --wandb-projectname CLD_FullSim_tagging --lr-scheduler reduceplateau
 else # train on multiple gpus (must have at least 4 input files)
-    torchrun --standalone --nnodes=1 --nproc_per_node=4 -m ./../weaver.train --data-train $datapath --data-config ./../configs/example.yaml --network-config ./../../particle_transformer/networks/example_ParticleTransformer.py --model-prefix ./model_weights/ --num-workers 0 --gpus 0 --batch-size 2048 --start-lr 1e-3 --num-epochs $NUM_EPOCH --optimizer ranger --fetch-step 0.01  --log-wandb --wandb-displayname test_fullsim_CLD --wandb-projectname CLD_FullSim_tagging --lr-scheduler reduceplateau --backend nccl
+    singularity exec -B /eos -B /afs --nv /afs/cern.ch/work/s/saaumill/public/colorsinglet.sif torchrun --standalone --nnodes=1 --nproc_per_node=4 "${dirpath}/weaver-core/weaver/train.py" --data-train $datapath --data-config "${dirpath}/weaver-core/configs/example.yaml" --network-config "${dirpath}/particle_transformer/networks/example_ParticleTransformer.py" --model-prefix ./model_weights/ --num-workers 0 --gpus 0 --batch-size 2048 --start-lr 1e-3 --num-epochs $NUM_EPOCH --optimizer ranger --fetch-step 0.01  --log-wandb --wandb-displayname test_fullsim_CLD --wandb-projectname CLD_FullSim_tagging --lr-scheduler reduceplateau --backend nccl
 fi
+
 end_time=$(date +%s)
 execution_time=$((end_time - start_time))
 # Calculate hours, minutes
